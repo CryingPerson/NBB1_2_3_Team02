f7eee8518b5d02e8974ac4e48087a4b2ca3608bd
CryingPerson <opnice12@naver.com>
2024-11-13 12:37:13 +0900

    CICD

--- src/test/kotlin/edu/example/kotlindevelop/KotlindevelopApplicationTests.kt
+++ src/test/kotlin/edu/example/kotlindevelop/KotlindevelopApplicationTests.kt
@@ -2,8 +2,10 @@ package edu.example.kotlindevelop
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
 
 @SpringBootTest
+@TestPropertySource(locations = ["classpath:/application-test.yml"])
 class KotlindevelopApplicationTests {
 
 	@Test
